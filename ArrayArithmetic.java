4724b31eea28067fc0cf083e2ccf2572abdb864f
Kamran Nazir <SheikhKamran1845@gmail.com>
2026-04-27 19:01:19 +0530

    Create ArrayArithmetic.java

---
+++ ArrayArithmetic.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class ArrayArithmetic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+        int max = arr[0], min = arr[0];
+        for(int i=1;i<n;i++){
+            if(arr[i] > max) max = arr[i];
+            if(arr[i] < min) min = arr[i];
+        }
+        double avg = (double)sum/n;
+        System.out.println(sum+" "+avg+" "+max+" "+min);
+    }
+}
