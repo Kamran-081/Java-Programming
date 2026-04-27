4e7f2c23a0d61f593c6ea34329593f30b730c922
Kamran Nazir <SheikhKamran1845@gmail.com>
2026-04-27 20:27:43 +0530

    Create ReverseEvenOdd.java

---
+++ ReverseEvenOdd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class ReverseEvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int even=0, odd=0;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            if(arr[i]%2==0) even++;
+            else odd++;
+        }
+        for(int i=n-1;i>=0;i--) System.out.print(arr[i]+" ");
+        System.out.println();
+        System.out.println(even+" "+odd);
+    }
+}
